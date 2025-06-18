ca7609a4fb124e19f97947fc875c6d59efbef0fb
Gladyson Rhuan <gladysonr04@gmail.com>
2025-06-18 15:52:34 -0300

    Feath: Foi feita a criação de Activity da Galeria

---
+++ app/src/main/java/com/grhuan/cat/presentation/GalleryActivity.kt
@@ -0,0 +1,21 @@
+package com.grhuan.cat.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.grhuan.cat.R
+import com.grhuan.cat.databinding.ActivityGalleryBinding
+
+class GalleryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGalleryBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityGalleryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
