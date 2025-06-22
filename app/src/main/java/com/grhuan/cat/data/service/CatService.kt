bd7727df037893465ff496f30d87eab98869e837
Gladyson Rhuan <gladysonr04@gmail.com>
2025-06-22 16:11:05 -0300

    Feath: Refatorando codigo

--- app/src/main/java/com/grhuan/cat/data/service/CatService.kt
+++ app/src/main/java/com/grhuan/cat/data/service/CatService.kt
@@ -1,9 +1,10 @@
 package com.grhuan.cat.data.service
 
 import com.grhuan.cat.data.model.CatResponse
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface CatService {
     @GET("images/search?size=small&has_breeds=true")
-    suspend fun getRandomCat(): List<CatResponse>
+    suspend fun getCat(): Response<List<CatResponse>>
 }
