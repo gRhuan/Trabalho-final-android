9734234f7f2b80ac08997f27f371147f835dc469
Gladyson Rhuan <gladysonr04@gmail.com>
2025-06-22 16:06:14 -0300

    Feath: Adicionando a função de excluir um item da lista

--- app/src/main/java/com/grhuan/cat/presentation/gallery/adapter/CatViewHolder.kt
+++ app/src/main/java/com/grhuan/cat/presentation/gallery/adapter/CatViewHolder.kt
@@ -1,16 +1,19 @@
-package com.grhuan.cat.presentation.adapter
+package com.grhuan.cat.presentation.gallery.adapter
 
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.grhuan.cat.data.model.CatEntity
-import com.grhuan.cat.data.model.CatResponse
 import com.grhuan.cat.databinding.ItemCatBinding
 
 class CatViewHolder(private val binding: ItemCatBinding): RecyclerView.ViewHolder(binding.root) {
-    fun bind(item: CatEntity){
+    fun bind(item: CatEntity, onClickListener: (String) -> Unit){
         Glide.with(binding.root.context)
             .load(item.url)
             .into(binding.ivIcon)
         binding.tvName.text = item.id
+
+        binding.root.setOnClickListener {
+            onClickListener.invoke(item.id)
+        }
     }
 }
