d8f861e43521c00e0970ae81065d2eab8537fba7
Gladyson Rhuan <gladysonr04@gmail.com>
2025-06-22 16:07:04 -0300

    Feath: Adicionando a função de excluir um item da lista

--- app/src/main/java/com/grhuan/cat/data/local/CatDao.kt
+++ app/src/main/java/com/grhuan/cat/data/local/CatDao.kt
@@ -1,24 +1,22 @@
-package com.grhuan.cat.data.local.dao
+package com.grhuan.cat.data.local
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.grhuan.cat.data.model.CatEntity
 
 @Dao
-interface Catdao {
+interface CatDao {
     // Insere um gato
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Insert(onConflict = OnConflictStrategy.Companion.REPLACE)
     suspend fun insert(cat: CatEntity)
 
     // Busca todos os gatos salvos
     @Query("SELECT * FROM cats")
     suspend fun getAll(): List<CatEntity>
 
-    // Deleta um gato
-    @Delete
-    suspend fun delete(cat: CatEntity)
+
+    @Query("DELETE FROM cats WHERE id = :catId")
+    suspend fun deleteById(catId: String)
 }
